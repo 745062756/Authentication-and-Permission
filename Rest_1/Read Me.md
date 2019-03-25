e60aa097b3bb3726fe7678483f0aa9ee94c1e22e
yida <tong.yida88@gmail.com>
2019-03-24 23:08:16 -0500

    File Create.

---
+++ Rest_1/Read Me.md
@@ -0,0 +1,9 @@
+## Package List (Newest Version):
+1. django 
+2. djangorestframework
+3. pygments
+
+**URL Tips**
+Run ......../snippets for List of existing instance in db
+Run ......../snippets/1 or 2 for Specific existing instance in db
+Because of luck of HTML file, you can't perform any request other than GET. 
--- Rest_1/snippets/models.py
+++ Rest_1/snippets/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from pygments.lexers import get_all_lexers
+from pygments.styles import get_all_styles
 
-# Create your models here.
+Lexers = [item for item in get_all_lexers() if item[1]]
+Language_Choice = sorted([(item[1][0], item[0]) for item in Lexers])
+Style_Choice = sorted((item, item) for item in get_all_styles())
+
+
+class Snippet(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    code = models.TextField()
+    linenos = models.BooleanField(default=False)
+    language = models.CharField(choices=Language_Choice, default='python', max_length=100)
+    style = models.CharField(choices=Style_Choice, default='friendly', max_length=100)
+
+    class Meta:
+        ordering = ('created',)
